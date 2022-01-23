8613407c038a32f875012545f5fd8405b09df807
minttea3 <3whtte@naver.com>
2022-01-23 09:02:55 +0900

    test1

--- src/main/java/com/projectwebs/ProjectWebsApplication.java
+++ src/main/java/com/projectwebs/ProjectWebsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectWebsApplication {
 
     public static void main(String[] args) {
+    	//test 11111
         SpringApplication.run(ProjectWebsApplication.class, args);
     }
 
